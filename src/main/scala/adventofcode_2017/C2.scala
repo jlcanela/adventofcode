56579d4cf38c49b8e2e687a7b0caba5898045783
Jean-Luc CANELA <jlcanelam@gmail.com>
2018-02-16 22:59:55 +0100

    Implement Challenge 1

---
+++ src/main/scala/adventofcode_2017/C2.scala
@@ -0,0 +1,9 @@
+package adventofcode_2017
+
+trait C2 {
+
+  def challenge2a(s: String) : Int = {
+    0
+  }
+
+}
---
+++ src/main/scala/adventofcode_2017/Challenge.scala
@@ -0,0 +1,10 @@
+package adventofcode_2017
+
+object Challenge {
+
+  def main(args: Array[String]): Unit = {
+    val c1 = new C1 {}
+    println(c1.challenge1b(c1.req))
+  }
+}
+
